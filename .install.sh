2659fd97dd262ba75545bc0dd3c7f0111d705bcb
raulpenate <50472064+raulpenate@users.noreply.github.com>
2024-08-29 13:10:23 -0600

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-yayapps="vim zsh brightnessctl python google-chrome visual-studio-code-bin discord"
-pacmanapps="vim bspwm sxhkd polybar picom alacritty nitrogen ranger nitrogen github-cli"
+yayapps="brightnessctl discord google-chrome python spotify visual-studio-code-bin"
+pacmanapps="alacritty bat bspwm github-cli go hugo nodejs neovim npm nitrogen obs-studio picom polybar python-pip ranger redshift sxhkd vim zsh"
 
 # Install 
-pacman -Syy $pacmanapps --noconfirm 
+sudo pacman -Syy $pacmanapps --noconfirm 
 yay -Syy $yayapps --noconfirm
+
+# Node
+sudo npm i -g gitmoji-cli
+
+# Restoring dotfiles
+sudo rsync -og --chown=$USER:$USER --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
+rm -rf tmpdotfiles
 
 # OH-MY-ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+chsh - s /bin/zsh
 
-# Create dotfiles
-rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
-rm -rf tmpdotfiles
-echo -e '\033[1m:: INSTALLATION COMPLETED!\033[0m'v
+# Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+# Chad NVIM
+git clone https://github.com/NvChad/starter ~/.config/nvim
+
+echo -e '\033[1m:: INSTALLATION COMPLETED!!\033[0m'
