624f6276f4fec60596130e289589f23e05b99826
raulpenate <50472064+raulpenate@users.noreply.github.com>
2025-06-13 21:15:43 -0600

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -1,31 +1,52 @@
 #!/bin/bash
-yayapps="brightnessctl discord google-chrome python spotify visual-studio-code-bin"
-pacmanapps="alacritty bat bspwm github-cli go hugo nodejs neovim npm nitrogen obs-studio picom polybar python-pip ranger redshift sxhkd vim zsh"
 
-# Install 
-sudo pacman -Syy $pacmanapps --noconfirm 
-yay -Syy $yayapps --noconfirm
+# Define package lists
+yay_apps="brightnessctl discord google-chrome python spotify visual-studio-code-bin librewolf"
+pacman_apps="alacritty arandr blueman sddm pulseaudio pavucontrol bat github-cli go hugo nodejs \
+thunar neovim npm nitrogen obs-studio picom polybar python-pip ranger redshift sxhkd vim zsh \
+rsync i3-wm i3lock networkmanager lxappearance"
 
-# Node
+# Install yay (AUR helper)
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay && makepkg -si && cd .. && rm -rf yay
+
+# Install packages from official repos and AUR
+sudo pacman -Syy --noconfirm $pacman_apps
+yay -Syy --noconfirm $yay_apps
+
+# Enable NetworkManager
+sudo systemctl enable --now NetworkManager
+
+# Global npm tools
 sudo npm i -g gitmoji-cli
 
-# Restoring dotfiles
-sudo rsync -og --chown=$USER:$USER --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
+# Restore dotfiles
+sudo rsync -av --chown="$USER:$USER" --exclude '.git' tmpdotfiles/ "$HOME/"
 rm -rf tmpdotfiles
 
-# OH-MY-ZSH
+# Restore fonts
+sudo cp -r ./.local/share/fonts /usr/share/fonts
+fc-cache -fv
+
+# Install Oh My Zsh (unattended)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+# Clone Zsh plugins # ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git "$ZSH_CUSTOM/plugins/fast-syntax-highlighting"
+git clone --depth 1 https://github.com/marlonrichert/zsh-autocomplete.git "$ZSH_CUSTOM/plugins/zsh-autocomplete"
 
-# Chad NVIM
+# Install NvChad starter config
 git clone https://github.com/NvChad/starter ~/.config/nvim
 
-# Change to zsh
-chsh - s /bin/zsh
-echo -e '\033[1m:: Installation completed!!\033[0m'
-echo -e '\033[1m:: PLEASE: REBOOT YOUR PC!!\033[0m'
+# Install SDDM theme
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
+
+# Change default shell to Zsh
+chsh -s /bin/zsh
+
+# Done
+echo -e '\033[1;32m:: Installation completed!!\033[0m'
+echo -e '\033[1;33m:: PLEASE: REBOOT YOUR PC!!\033[0m'
